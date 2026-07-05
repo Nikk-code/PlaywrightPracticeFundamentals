745ebb2677a3690118da27a341eed3c78e814f7f
Nikk-code <nikkhilsakalkar@gmail.com>
2026-07-05 23:19:29 +0530

    Using Json

--- tests/login/login.spec.js
+++ tests/login/login.spec.js
@@ -2,11 +2,10 @@ const { test } = require('../../fixtures/baseFixture')
 const { LoginPage } = require('../../pages/LoginPage')
 const { WaitHelper } = require('../../helpers/WaitHelper')
 const { ExcelUtil } = require('../../utils/ExcelUtil')
+const data = require('../../test-data/users.json')
 
 
 test('Login', async ({ loginPage, page }) => {
-
-    const login = new LoginPage(page);
 
     const users = ExcelUtil.read('./test-data/users.xlsx')
     console.log(users)
@@ -33,6 +32,23 @@ test('Login', async ({ loginPage, page }) => {
     // page.getByLabel('Username')
 
     // page.getByPlaceholder('Password')
+
+}
+
+)
+
+
+test('Login With JSON', async ({ loginPage, page }) => {
+
+    const login = new LoginPage(page);
+
+    console.log(data.user)
+
+    await loginPage.navigate();
+
+    await loginPage.loginUser(data.user, data.password);
+
+    await WaitHelper.visible(page.locator('#inventory_container').first());
 
 }
 
