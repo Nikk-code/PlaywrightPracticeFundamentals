7a1b1aebe03f78847931cb0f277df463be59bfe7
Nikk-code <nikkhilsakalkar@gmail.com>
2026-08-13 21:52:38 +0530

    Create Post request using json file body

---
+++ tests/APIValidation/post_api_Request_02_creatBooking.spec.ts
@@ -0,0 +1,55 @@
+/*
+Test: create booking
+Request type: Post
+Request body: JSON file
+
+Add url to playwright.config.ts file
+    baseURL: 'https://restful-booker.herokuapp.com'
+ 
+
+*/
+
+import { test, expect, request } from "@playwright/test"
+import fs from 'fs';
+
+test('Create Post request using json file body', async ({ request }) => {
+
+    //read data from json (request body)
+
+    const jsonFile = "./test-data/post_request_body.json";
+    const requestBody = JSON.parse(fs.readFileSync(jsonFile, 'utf-8'));
+
+    // send post request
+    const response = await request.post("/booking", { data: requestBody });
+    const responseBody = await response.json();  //Extracted response
+    console.log(responseBody);
+
+    //verify reponse
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+
+    // validate response body attributes
+    expect(responseBody).toHaveProperty("bookingid");
+    expect(responseBody).toHaveProperty("booking");
+    expect(responseBody).toHaveProperty("booking.additionalneeds");
+
+    //validate booking details
+
+    //validate booking details
+    const booking = responseBody.booking;
+
+    expect(booking).toMatchObject({
+        firstname: requestBody.firstname,
+        lastname: requestBody.lastname,
+        totalprice: requestBody.totalprice,
+        depositpaid: requestBody.depositpaid,
+        additionalneeds: requestBody.additionalneeds
+    });
+
+    //validate booking dates (nested json object)
+
+    expect(booking.bookingdates).toMatchObject({
+        checkin: requestBody.bookingdates.checkin,
+        checkout: requestBody.bookingdates.checkout,
+    });
+})
